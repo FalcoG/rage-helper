6080a868d4bb3f94883bd519128af7e9764e5fc5
Falco Gerritsjans <fcb319@alias.falco.pro>
2021-10-20 19:51:57 +0200

    fix: define types in the same place as functions

--- src/bus-stops.ts
+++ src/bus-stops.ts
@@ -1,5 +1,21 @@
 import busStopsData from '../gta-v-data-dumps/objectslocations/worldBusStops.json'
 
+type BusStops = BusStop[]
+
+interface BusStop {
+  Name: string
+  Position: {
+    X: number
+    Y: number
+    Z: number
+  }
+  Rotation: {
+    X: number
+    Y: number
+    Z: number
+  }
+}
+
 const busStops: BusStops = busStopsData
 
 export default busStops
